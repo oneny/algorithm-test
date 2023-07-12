b15fabe20a1291de06df3137a309c71c4097cb48
oneny <97153666+oneny@users.noreply.github.com>
2023-07-12 13:24:45 +0900

    [Gold V] Title: 탑, Time: 904 ms, Memory: 101860 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Stack<Top> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            Top top = new Top(Integer.parseInt(st.nextToken()), i + 1);
+
+            while (!stack.isEmpty() && stack.peek().height < top.height) {
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                sb.append(stack.peek().value + " ");
+            } else {
+                sb.append("0 ");
+            }
+
+            stack.add(top);
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
+
+class Top {
+
+    int height;
+    int value;
+
+    public Top(int height, int value) {
+        this.height = height;
+        this.value = value;
+    }
+}
