668df3822c2659c5fbadfc58c505fc6e8a3be03e
oneny <97153666+oneny@users.noreply.github.com>
2023-07-13 16:53:40 +0900

    Time: 4 ms (86.52%), Space: 39.4 MB (99.44%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.java
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.java
@@ -1,17 +1,30 @@
 class Solution {
-    int result;
+    
+    int ans;
+    
     public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
-        result = baseCosts[0];
-        for(int base: baseCosts)
-            helper(base, toppingCosts, 0, target);
-        return result;
+        ans = baseCosts[0];
+        
+        for (int baseCost : baseCosts) { // baseCosts를 돌면서 target에 근접한 합계 계산
+            dfs(baseCost, 0, target, toppingCosts);
+        }
+        
+        return ans;
     }
-    private void helper(int current, int[] toppingCosts, int index, int target) {
-        if( Math.abs(target - current) < Math.abs(target - result) || Math.abs(target - current) == Math.abs(target - result) && current < result)
-            result = current;
-        if(index==toppingCosts.length || current >= target) return;
-        helper(current, toppingCosts, index + 1, target);
-        helper(current + toppingCosts[index], toppingCosts, index + 1, target);
-        helper(current + toppingCosts[index]*2, toppingCosts, index + 1, target);
+    
+    private void dfs(int currentCost, int i, int target, int[] toppingCosts) {
+        // 두 차이 절대값이 같으면 낮은 currentCost를 선택
+        if (Math.abs(target - currentCost) < Math.abs(target - ans) || (Math.abs(target - currentCost) == Math.abs(target - ans) && currentCost <= target)) {
+            ans = currentCost;
+        }
+        
+
+        if (i == toppingCosts.length || currentCost >= target) {
+            return;
+        }
+
+        dfs(currentCost, i + 1, target, toppingCosts);
+        dfs(currentCost + toppingCosts[i], i + 1, target, toppingCosts);
+        dfs(currentCost + toppingCosts[i] * 2, i + 1, target, toppingCosts);
     }
 }
