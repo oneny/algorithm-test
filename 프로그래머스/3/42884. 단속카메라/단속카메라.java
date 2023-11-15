b17856c60c14fd4e428d79d606f91dbf4f8a1cf6
oneny <97153666+oneny@users.noreply.github.com>
2023-11-15 09:16:55 +0900

    [level 3] Title: 단속카메라, Time: 9.20 ms, Memory: 53.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
+        
+        int cam = Integer.MIN_VALUE;
+        
+        for (int[] route : routes) {
+            
+            if (cam < route[0]) {
+                cam = route[1];
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
