9c5a380c667bd2a2dcc63a4e84533b2f3c4be689
oneny <gjwodnjs1996@gmail.com>
2024-01-15 20:04:55 +0900

    [level Lv.1] Title: [1차] 비밀지도, Time: 8.28 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+        }
+    
+        System.out.println(Arrays.toString(result));
+
+        for (int i = 0; i < n; i++) {
+            result[i] = String.format("%" + n + "s", result[i]);
+            result[i] = result[i].replaceAll("1", "#");
+            result[i] = result[i].replaceAll("0", " ");
+        }
+
+        return result;
+    }
+}
